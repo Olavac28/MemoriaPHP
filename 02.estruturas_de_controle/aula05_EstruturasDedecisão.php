167a212a9ba4ffb6763bbd9f589b20c518723715
Hans <hans28h@gmail.com>
2025-10-11 23:04:43 -0300

    estruturas de controle

---
+++ 02.estruturas_de_controle/aula05_EstruturasDedecisão.php
@@ -0,0 +1,7 @@
+<!--if, else e switch igual C quando puro, mas rodado diferente quando misturado com HTML:-->
+
+<?php $nota = 7; if ($nota >= 7): ?>
+    <h1>Voce Passou</h1>
+<?php else: ?>
+    <h1>Voce reprovou</h1>
+<?php endif; ?>
---
+++ 02.estruturas_de_controle/aula06_EstruturasDeRepeticao.php
@@ -0,0 +1,8 @@
+<?php
+    //while, do while e for, iguais o C e tendo a forma em junção ao HTML
+
+    $cores = ["azul", "vermelho", "verde"];
+    foreach($cores as $c) {
+        echo "$c<br>";
+    }
+?>
